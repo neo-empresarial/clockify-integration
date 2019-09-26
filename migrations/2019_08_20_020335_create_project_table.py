f86bd2cf4190b73c3e3f3ccf8c6292259a754581
José Ribeiro <ribeirojose@me.com>
2019-09-26 09:25:21 -0300

    Add client_id column to the project table

--- migrations/2019_08_20_020335_create_project_table.py
+++ migrations/2019_08_20_020335_create_project_table.py
@@ -2,19 +2,20 @@ from orator.migrations import Migration
 
 
 class CreateProjectTable(Migration):
-
     def up(self):
         """
         Run the migrations.
         """
-        with self.schema.create('project') as table:
-            table.increments('id')
-            table.string('clockify_id').unique().nullable()
-            table.string('name')
+        with self.schema.create("project") as table:
+            table.increments("id")
+            table.string("clockify_id").unique().nullable()
+            table.string("name")
+            table.integer("client_id").unsigned()
+            table.foreign("client_id").references("id").on("client")
             table.timestamps()
 
     def down(self):
         """
         Revert the migrations.
         """
-        self.schema.drop('project')
+        self.schema.drop("project")
