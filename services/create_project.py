7dcbfabb3c9a979d4ec969193cf95c0a2da9bd4a
José Ribeiro <ribeirojose@me.com>
2019-11-18 17:44:49 -0300

    Simplify project creation logic

--- services/create_project.py
+++ services/create_project.py
@@ -20,7 +20,7 @@ def create_client(name: AnyStr) -> None:
 
 @click.command()
 @click.option("--name", prompt="Project name", help="The name for the project.")
-# @click.option("--client", prompt="Client name", help="The client name for the project.")
+# TODO: implement clients in our DB
 def create_project(name: AnyStr) -> None:
     colors = {
         "e": "#69b39e",
@@ -29,13 +29,15 @@ def create_project(name: AnyStr) -> None:
         "w": "#1b60ce",
         "c": "#8BC34A",
     }
+
     project_name = name.upper()
     default_activities = Project.find(0).activities
     proj_data = {
         "name": project_name,
-        "isPublic": "false",
+        "isPublic": "true",  # On Clockify this means the project is visible to the whole team
         "billable": "true",
-        "color": colors[name[0]],
+        "color": colors[project_name.lower()[0]],
+        "tasks": [{"name": x.name} for x in default_activities.all()],
     }
     proj_response = requests.post(
         f"{V1_API_URL}/workspaces/{WORKSPACE_ID}/projects",
@@ -44,16 +46,9 @@ def create_project(name: AnyStr) -> None:
     )
     proj_clockify_id = proj_response.json().get("id")
 
-    for activity in default_activities.all():
-        ac_data = {"name": activity.name, "projectId": proj_clockify_id}
-        requests.post(
-            f"{V1_API_URL}/workspaces/{WORKSPACE_ID}/projects/{proj_clockify_id}/tasks",
-            json=ac_data,
-            headers=HEADERS,
-        )
-
     new_proj = Project.create(name=project_name, clockify_id=proj_clockify_id)
     new_proj.activities().sync(default_activities.map(lambda x: x.id).all())
+
     print(f"Project {project_name} created.")
     pass
 
