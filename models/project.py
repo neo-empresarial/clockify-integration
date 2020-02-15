0779d9227d94e2329461d0299156c87f5af93291
Fábio Mendes <ms.fabiomendes@gmail.com>
2020-02-15 20:56:13 -0300

    Add archived parameter and increase page-size request

--- models/project.py
+++ models/project.py
@@ -1,7 +1,5 @@
 from models import *
-
 from orator.orm import belongs_to_many
-
 import requests
 
 
@@ -39,13 +37,14 @@ class Project(Model):
         return projects
 
     @staticmethod
-    def fetch_all_projects():
+    def fetch_all_projects(archived=0):
         """Find all projects on NEO's workspace.
-
+        Use the parameter archived=1 to retrieve all projects.
         Returns list of dictionaries containing "name", "clockify_id"
         for every project."""
 
-        url = "{}/workspaces/{}/projects".format(V1_API_URL, WORKSPACE_ID)
+        url = "{}/workspaces/{}/projects?page-size=100&archived={}"
+        url = url.format(V1_API_URL, WORKSPACE_ID, archived)
         responses = requests.get(url, headers=HEADERS)
         return [
             {"name": project["name"].lower(), "clockify_id": project["id"]}
