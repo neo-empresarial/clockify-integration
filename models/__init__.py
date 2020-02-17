f3498f4387f780d1a7a1517c05df864176bc549d
Lukas Alberto Belck <lukas.belck@gmail.com>
2020-02-17 00:37:21 -0300

    Add V0 API URL

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 from orator import DatabaseManager, Model
 import os
 
+V0_API_URL = "https://api.clockify.me/api"
 V1_API_URL = "https://api.clockify.me/api/v1"
 WORKSPACE_ID = os.getenv("CLOCKIFY_WORKSPACE_ID")
 HEADERS = {
