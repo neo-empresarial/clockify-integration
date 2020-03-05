87eb4080ea8987b1ff516e2a7e730b24a63420de
Lukas Alberto Belck <lukas.belck@gmail.com>
2020-03-05 20:33:27 -0300

    Add date of when timesheet structured changed

--- services/populate.py
+++ services/populate.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     Client.save_from_clockify()
     Project.save_from_clockify(archived="")
     Activity.save_from_clockify()
-    TimeEntry.save_from_clockify()
+    TimeEntry.save_from_clockify(start="2020-03-01T00:00:01Z")
