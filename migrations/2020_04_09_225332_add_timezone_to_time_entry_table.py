32592a60e1e1084a9518b598e872f06632507494
Lukas Alberto Belck <lab@neo.ufsc.br>
2020-04-11 21:36:03 -0300

    Remove unecessary with statment

--- migrations/2020_04_09_225332_add_timezone_to_time_entry_table.py
+++ migrations/2020_04_09_225332_add_timezone_to_time_entry_table.py
@@ -6,16 +6,14 @@ class AddTimezoneToTimeEntryTable(Migration):
         """
         Run the migrations.
         """
-        with self.schema.table('time_entry') as table:
-            self.db.update("SET timezone='UTC';")
-            self.db.update('ALTER TABLE time_entry ALTER COLUMN "start" TYPE TIMESTAMP WITH TIME ZONE;')
-            self.db.update('ALTER TABLE time_entry ALTER COLUMN "end" TYPE TIMESTAMP WITH TIME ZONE;')
+        self.db.update("SET timezone='UTC';")
+        self.db.update('ALTER TABLE time_entry ALTER COLUMN "start" TYPE TIMESTAMP WITH TIME ZONE;')
+        self.db.update('ALTER TABLE time_entry ALTER COLUMN "end" TYPE TIMESTAMP WITH TIME ZONE;')
 
     def down(self):
         """
         Revert the migrations.
         """
-        with self.schema.table('time_entry') as table:
-            self.db.update("SET timezone='UTC';")
-            self.db.update('ALTER TABLE time_entry ALTER COLUMN "start" TYPE TIMESTAMP WITHOUT TIME ZONE;')
-            self.db.update('ALTER TABLE time_entry ALTER COLUMN "end" TYPE TIMESTAMP WITHOUT TIME ZONE;')
+        self.db.update("SET timezone='UTC';")
+        self.db.update('ALTER TABLE time_entry ALTER COLUMN "start" TYPE TIMESTAMP WITHOUT TIME ZONE;')
+        self.db.update('ALTER TABLE time_entry ALTER COLUMN "end" TYPE TIMESTAMP WITHOUT TIME ZONE;')
